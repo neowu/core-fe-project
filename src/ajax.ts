365df66372a137fc3644cfe955a23a15187585e2
neo <neowu.us@gmail.com>
2018-07-05 13:23:17 -0400

    add type safety callAJAX, release 0.1.0

--- src/ajax.ts
+++ src/ajax.ts
@@ -1,5 +1,6 @@
 import axios, {AxiosError, AxiosRequestConfig} from "axios";
 import {Exception} from "./exception";
+import {call, CallEffect} from "redux-saga/effects";
 
 export class APIException extends Exception {
     constructor(message: string, public statusCode: number | null, public requestURL: string, public responseData: object | null) {
@@ -66,3 +67,42 @@ export function url(pattern: string, params: object): string {
     });
     return url;
 }
+
+interface CallAJAXEffect<R> extends CallEffect {
+    response: () => R;
+}
+
+type Function0<R> = () => Promise<R>;
+type Function1<R, T1> = (arg1: T1) => Promise<R>;
+type Function2<R, T1, T2> = (arg1: T1, arg2: T2) => Promise<R>;
+type Function3<R, T1, T2, T3> = (arg1: T1, arg2: T2, arg3: T3) => Promise<R>;
+type Function4<R, T1, T2, T3, T4> = (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<R>;
+
+interface CallAJAX {
+    <R>(fn: Function0<R>): CallAJAXEffect<R>;
+
+    <R, T1, A1 extends T1>(fn: Function1<R, T1>, arg1: A1): CallAJAXEffect<R>;
+
+    <R, T1, T2, A1 extends T1, A2 extends T2>(fn: Function2<R, T1, T2>, arg1: A1, arg2: A2): CallAJAXEffect<R>;
+
+    <R, T1, T2, T3, A1 extends T1, A2 extends T2, A3 extends T3>(fn: Function3<R, T1, T2, T3>, arg1: A1, arg2: A2, arg3: A3): CallAJAXEffect<R>;
+
+    <R, T1, T2, T3, T4, A1 extends T1, A2 extends T2, A3 extends T3, A4 extends T4>(fn: Function4<R, T1, T2, T3, T4>, arg1: A1, arg2: A2, arg3: A3, arg4: A4): CallAJAXEffect<R>;
+}
+
+export const callAJAX: CallAJAX = (func: (...args: any[]) => Promise<any>, ...args: any[]) => {
+    let response: any;
+    const proxy = (...args: any[]) => {
+        return func(...args)
+            .then(result => {
+                response = result;
+                return response;
+            })
+            .catch(reject => {
+                throw reject;
+            });
+    };
+    const effect: CallAJAXEffect<any> = (call as any)(proxy, ...args);
+    effect.response = () => response;
+    return effect;
+};
--- test/ajax.test.ts
+++ test/ajax.test.ts
@@ -1,4 +1,4 @@
-import {json, url} from "ajax";
+import {callAJAX, json, url} from "ajax";
 
 test("json", () => {
     expect(json('{"date": "2018-05-24T12:00:00.000Z"}')).toEqual({date: new Date("2018-05-24T12:00:00.000Z")});
@@ -8,4 +8,17 @@ test("url", () => {
     expect(url("/user", {})).toEqual("/user");
     expect(url("/user/:id", {id: 1})).toEqual("/user/1");
     expect(url("/user/:userId/item/:itemId", {userId: "1", itemId: "2"})).toEqual("/user/1/item/2");
+});
+
+test("callAJAX", () => {
+    const ajax = (arg: string): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            return resolve("result");
+        });
+    };
+
+    const effect = callAJAX(ajax, "argument");
+    expect(effect.CALL).toBeDefined();
+    expect(effect.CALL.args).toEqual(["argument"]);
+    expect(effect.response).toBeInstanceOf(Function);
 });
