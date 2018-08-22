8ab964aa43df0124c6e402e82da75abd665c22c1
neo <neowu.us@gmail.com>
2018-08-21 20:55:01 -0400

    tweak setState action type, to avoid conflict with handle actions

--- src/action/handler.ts
+++ src/action/handler.ts
@@ -26,11 +26,11 @@ export class Handler<S extends object, R extends State = State> {
     }
 
     protected *resetState(): SagaIterator {
-        yield put(setStateAction(this.module, this.initialState, `${this.module}/resetState`));
+        yield put(setStateAction(this.module, this.initialState, `@@${this.module}/resetState`));
     }
 
     protected *setState(newState: Partial<S>, type: string = "setState"): SagaIterator {
-        yield put(setStateAction(this.module, newState, `${this.module}/${type}`));
+        yield put(setStateAction(this.module, newState, `@@${this.module}/${type}`));
     }
 }
 
