50e6288725dcd54d1d36f5e37b359e165b91877e
neo <neowu.us@gmail.com>
2018-09-27 13:57:42 -0400

    update to typescript 3.1.1

--- src/util/Clipboard.ts
+++ src/util/Clipboard.ts
@@ -8,14 +8,16 @@ export class Clipboard {
         document.body.appendChild(element);
 
         // In case there is already some text selection by user, store the previous selection & recover later
-        const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+        const selection = document.getSelection();
+        const selected = selection != null && selection.rangeCount > 0 ? selection.getRangeAt(0) : null;
+
         element.select();
         document.execCommand("copy");
         document.body.removeChild(element);
 
-        if (selected) {
-            document.getSelection().removeAllRanges();
-            document.getSelection().addRange(selected);
+        if (selection && selected) {
+            selection.removeAllRanges();
+            selection.addRange(selected);
         }
     }
 }
