1a2e7d953adc5d9e4ca905de120252cce121616a
Dion <dion.shi.hk@gmail.com>
2018-12-04 13:53:40 +0800

    Fix strict function call issue for sagaCall.

--- src/call.ts
+++ src/call.ts
@@ -4,20 +4,19 @@ interface CallWithResultEffect<R> extends CallEffect {
     result: () => R;
 }
 
-export const call = <R, P extends any[]>(func: (...args: P) => Promise<R>, ...args: P) => {
+export const call = <R, P extends any[]>(fn: (...args: P) => Promise<R>, ...args: P) => {
     let result: R;
-    const effect: CallWithResultEffect<R> = sagaCall.call(
-        null,
-        (...args: P) =>
-            func(...args).then(value => {
-                result = value;
-                return result;
-            }),
-        ...args
-    );
+    // Same as fn (parameter), but store its promised return into "result"
+    const wrappedFn = (...args: P) => {
+        return fn(...args).then(_ => {
+            result = _;
+            return _;
+        });
+    };
+    const effect = sagaCall.apply(null, [wrappedFn, ...args] as any) as CallWithResultEffect<R>;
     effect.result = () => {
         if (result === undefined) {
-            throw new Error("result is undefined, please yield effect before calling result()");
+            throw new Error("Effect has not been yielded");
         }
         return result;
     };
