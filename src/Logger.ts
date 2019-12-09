379720f2d192e0d6af330779123ab974a99a0601
dionshi <dion.shi.hk@gmail.com>
2019-12-09 13:32:51 +0800

    1.13.2: Treat API 421 error as warning.

--- src/Logger.ts
+++ src/Logger.ts
@@ -96,7 +96,7 @@ export class LoggerImpl implements Logger {
             if (exception instanceof APIException) {
                 errorCode = `API_ERROR_${exception.statusCode}`;
                 // Following cases are treated as expected
-                if ([401, 403, 426, 503].includes(exception.statusCode)) {
+                if ([401, 403, 421, 426, 503].includes(exception.statusCode)) {
                     isWarning = true;
                 } else if (exception.statusCode === 400) {
                     if (exception.errorCode === "VALIDATION_ERROR") {
