390e590edb38a4d9e323e010232cc434d2feedbe
dionshi <dion.shi.hk@gmail.com>
2020-08-21 10:23:59 +0800

    1.20.1: Upgrade TypeScript 4, and update useModuleAction/useModuleObjectAction hooks.

--- src/hooks.ts
+++ src/hooks.ts
@@ -7,10 +7,34 @@ export function useLoadingStatus(identifier: string = "global"): boolean {
 }
 
 /**
- * Action parameters must be of primitive types, so that the dependency check can work well.
- * No need add dispatch to dep list, because it is always fixed.
+ * For actions like:
+ * *foo(a: number, b: string, c: boolean): SagaIterator {..}
+ *
+ * useModuleAction(foo) will return:
+ * (a: number, b: string, c: boolean) => void;
+ *
+ * useModuleAction(foo, 100) will return:
+ * (b: string, c: boolean) => void;
+ *
+ * useModuleAction(foo, 100, "") will return:
+ * (c: boolean) => void;
+ *
+ * useModuleAction(foo, 100, "", true) will return:
+ * () => void;
  */
-export function useModuleAction<P extends Array<string | number | boolean | null | undefined>>(actionCreator: (...args: P) => Action<P>, ...deps: P): () => void {
+export function useModuleAction<T extends Array<string | number | boolean | null | undefined>, U extends any[]>(actionCreator: (...args: [...T, ...U]) => Action<[...T, ...U]>, ...deps: T): (...args: U) => void {
     const dispatch = useDispatch();
-    return React.useCallback(() => dispatch(actionCreator(...deps)), deps);
+    return React.useCallback((...args: U) => dispatch(actionCreator(...deps, ...args)), [dispatch, actionCreator, ...deps]);
+}
+
+/**
+ * For actions like:
+ * *foo(data: {key: number}): SagaIterator {..}
+ *
+ * useModuleObjectAction(foo, "key") will return:
+ * (objectValue: number) => void;
+ */
+export function useModuleObjectAction<T extends object, K extends keyof T>(actionCreator: (arg: T) => Action<[T]>, objectKey: K): (objectValue: T[K]) => void {
+    const dispatch = useDispatch();
+    return React.useCallback((objectValue: T[K]) => dispatch(actionCreator({[objectKey]: objectValue} as T)), [dispatch, actionCreator, objectKey]);
 }
