14cc3a55def57d8eabe4ed2f816dad1ec0e9df4f
ianlau <ianlauyin@gmail.com>
2024-07-31 17:51:06 +0800

    updated `attachLifecycle` with actions props

--- src/platform/ModuleProxy.tsx
+++ src/platform/ModuleProxy.tsx
@@ -21,20 +21,7 @@ export class ModuleProxy<M extends Module<any, any>> {
         return this.actions;
     }
 
-    attachLifecycleWithActions<P extends object>(ComponentType: React.ComponentType<P & {actions: ActionCreators<M>}>): React.ComponentType<P> {
-        const LifecycleComponent = this.attachLifecycle(ComponentType);
-        const actions = this.actions;
-        return class extends React.PureComponent<P> {
-            constructor(props: P) {
-                super(props);
-            }
-            override render() {
-                return <LifecycleComponent {...this.props} actions={actions} />;
-            }
-        };
-    }
-
-    attachLifecycle<P extends object>(ComponentType: React.ComponentType<P>): React.ComponentType<P> {
+    attachLifecycle<P extends object>(ComponentType: React.ComponentType<P & {actions: ActionCreators<M>}>): React.ComponentType<P> {
         const moduleName = this.module.name as string;
         const lifecycleListener = this.module as ModuleLifecycleListener;
         const modulePrototype = Object.getPrototypeOf(lifecycleListener);
@@ -128,7 +115,7 @@ export class ModuleProxy<M extends Module<any, any>> {
             }
 
             override render() {
-                return <ComponentType {...this.props} />;
+                return <ComponentType {...this.props} actions={actions} />;
             }
 
             private areLocationsEqual = (a: Location, b: Location): boolean => {
